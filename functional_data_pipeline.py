ebfd8560d149a5f4202faba2f02a919a8c74ee0a
michealcoleyoung <coleyoungmusic@gmail.com>
2024-05-28 18:54:53 -0500

    Updated functions

--- functional_data_pipeline.py
+++ functional_data_pipeline.py
@@ -1,38 +1,56 @@
-users = [
-    {"id": 1, "name": "Alice", "age": 28, "email": "alice@example.com"},
-    {"id": 2, "name": "Bob", "age": 22, "email": "bob@example.com"},
-    {"id": 3, "name": "Charlie", "age": 35, "email": "charlie@example.com"},
-    {"id": 4, "name": "David", "age": 23, "email": "david@example.com"},
-]
+import json
 
-def filter_by_age(user):
+
+def filter_by_age(data):
     # returns only users that are 25 and older
     filtered_users = []
-    for info in user:
-        age = info["age"]
+    for user in data:
+        age = user["age"]
         if age >= 25:
-            filtered_users.append(info)
+            filtered_users.append(user)
     return filtered_users
 
 
-def transform_names(user):
+def transform_names(data):
     # returns all names in uppercase
-    uppercase_names = []
-    for info in user:
-        name = info["name"].upper()
-        uppercase_names.append(name)
-    return uppercase_names
+    # uppercase_names = []
+    # counter = -1
+    # for user in data:
+    #     name = user["name"].upper()
+    #     uppercase_names.append(name)
+    # for name in uppercase_names:
+    #     counter += 1
+    #     data[counter]["name"] = name
+    # return data
+    pass
 
 
-def extract_emails(user):
+def extract_emails(data):
     # returns all email addresses
     email_address = []
-    for info in user:
-        email = info["email"]
+    for user in data:
+        email = user["email"]
         email_address.append(email)
     return email_address
 
 
-print(filter_by_age(users))
-print(transform_names(users))
-print(extract_emails(users))
+def compose_functions(*functions):
+    def composed_function(data):
+        for user in reversed(functions):
+            data = user(data)
+        return data
+    return composed_function
+
+
+with open('data/users.json') as f:
+    users = json.load(f)
+
+
+print(f"Filter By Age: {filter_by_age(users)}")
+print(f"Transform Names: {transform_names(users)}")
+print(f"Extract Emails: {extract_emails(users)}")
+
+
+
+
+
